daa9ba8a62e3e43085bf20960be0fd0be06d28eb
Travis Watson <watsontravis@protonmail.com>
2023-02-12 07:31:29 -0600

    Add movies service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,20 +7,12 @@ import { HeaderComponent } from './shared/header/header.component';
 import { FooterComponent } from './shared/footer/footer.component';
 import { HomeComponent } from './pages/home/home.component';
 import { MoviesComponent } from './pages/movies/movies.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent,
-    FooterComponent,
-    HomeComponent,
-    MoviesComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, HeaderComponent, FooterComponent, HomeComponent, MoviesComponent],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/services/movies.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MoviesService {
+  constructor(private http: HttpClient) {}
+
+  getMovies() {
+    return this.http.get(
+      'https://api.themoviedb.org/3/movie/popular?api_key=25e89abda780117ddaefd79119ab7f7e'
+    );
+  }
+}
