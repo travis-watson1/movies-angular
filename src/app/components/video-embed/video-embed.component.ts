447694dbe457b93d9671ea4be27e4c03410fcc55
Travis Watson <james.t.watson@saic.com>
2023-03-01 13:32:12 -0600

    Add video component

--- src/app/components/video-embed/video-embed.component.ts
+++ src/app/components/video-embed/video-embed.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { DomSanitizer } from '@angular/platform-browser';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-video-embed',
@@ -9,9 +9,20 @@ import { DomSanitizer } from '@angular/platform-browser';
 export class VideoEmbedComponent implements OnInit {
   @Input() site: string = 'YouTube';
   @Input() key: string | null = null;
+
+  videoUrl: SafeResourceUrl = '';
   constructor(private sanitizer: DomSanitizer) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    switch (this.site) {
+      case 'YouTube':
+        this.videoUrl = this.getSafeUrl('https://www.youtube.com/embed/' + this.key);
+        break;
+      case 'Vimeo':
+        this.videoUrl = '';
+        break;
+    }
+  }
 
   getSafeUrl(url: string) {
     return this.sanitizer.bypassSecurityTrustResourceUrl(url);
