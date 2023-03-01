62950434c2ef90349bf44e0be6258e8723647bfb
Travis Watson <james.t.watson@saic.com>
2023-03-01 12:35:48 -0600

    Add summary movie details tab

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { ItemsBannerComponent } from './components/items-banner/items-banner.com
 import { ItemComponent } from './components/item/item.component';
 import { PaginatorModule } from 'primeng/paginator';
 import { MovieComponent } from './pages/movie/movie.component';
+import { TabViewModule } from 'primeng/tabview';
 
 @NgModule({
   declarations: [
@@ -27,7 +28,7 @@ import { MovieComponent } from './pages/movie/movie.component';
     ItemComponent,
     MovieComponent
   ],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule, PaginatorModule],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, PaginatorModule, TabViewModule],
   providers: [],
   bootstrap: [AppComponent]
 })
